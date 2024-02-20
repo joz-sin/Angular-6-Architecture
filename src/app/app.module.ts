70090f10ef40e3ace19c7b492bb733c8df6daf85
Unpaid Worker <74233190+teamweb095@users.noreply.github.com>
2024-02-20 23:51:47 +0530

    Angular upgrade changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { CoreModule }     from './core/core.module';
-import { AppRoutingModule }     from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
 
@@ -18,23 +18,17 @@ import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButt
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
-
     BrowserAnimationsModule,
-
     MatGridListModule,
-
     MatCardModule,
-
     MatMenuModule,
-
     MatIconModule,
-
     MatButtonModule
   ],
   declarations: [
     AppComponent,
     DashboardComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
